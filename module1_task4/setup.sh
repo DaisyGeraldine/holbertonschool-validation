99ca5c0e10f89fd8b6dea9a2f6840bcb0bf7ff4f
DaisyGeraldine <chipanal@hotmail.com>
2023-01-05 19:54:28 -0500

    fix: modify setup.sh file to correct installation

--- module1_task4/setup.sh
+++ module1_task4/setup.sh
@@ -3,7 +3,9 @@
 
 ## * The production website is hosted in an Ubuntu 18.04 Docker container
 ## * The applications "GoHugo" and "Make" are installed with `apt-get update && apt-get install -y hugo`.
-apt-get update && apt-get install -y hugo
-apt-get install -y make
+sudo apt-get update
+sudo apt-get install -y hugo
+## docker pull klakegg/hugo
+sudo apt-get install -y make
 ## *  When running the command `make build`, there is a bunch of errors which end with the following lines:
 make build
