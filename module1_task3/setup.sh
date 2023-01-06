4e92773085e7710eec909be80d1c28406fe5c520
DaisyGeraldine <chipanal@hotmail.com>
2023-01-05 19:10:57 -0500

    update file setup.sh

--- module1_task3/setup.sh
+++ module1_task3/setup.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 ## Production Instructions
 
-# * The production website is hosted in an Ubuntu 18.04 Docker container
-# * The applications "GoHugo" and "Make" are installed with `apt-get update && apt-get install -y hugo make`.
-# *  When running the command `make build`, there is a bunch of errors which end with the following lines:
-
-# Error: Error building site: logged 15 error(s)
-echo "recipe for target 'build' failed"
-# make: *** [build] Error 255
+## * The production website is hosted in an Ubuntu 18.04 Docker container
+## * The applications "GoHugo" and "Make" are installed with `apt-get update && apt-get install -y hugo make`.
+apt-get update && apt-get install -y hugo make
+## *  When running the command `make build`, there is a bunch of errors which end with the following lines:
+make build
+## Error: Error building site: logged 15 error(s)
+## echo "recipe for target 'build' failed"
+## exit 255
