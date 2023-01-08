5b02156604a9725448110afd0a3fd759dd066d12
DaisyGeraldine <chipanal@hotmail.com>
2023-01-08 00:04:58 -0500

    fix: modify installation

--- module3_task1/setup.sh
+++ module3_task1/setup.sh
@@ -8,7 +8,7 @@ apt upgrade
 apt install -y wget
 apt install -y make
 wget https://github.com/gohugoio/hugo/releases/download/v0.84.0/hugo_extended_0.84.0_Linux-64bit.deb
-dpkg -i hugo_extended_0.84.0_Linux-64bit.deb
+sudo dpkg -i hugo_extended_0.84.0_Linux-64bit.deb
 # docker pull klakegg/hugo
 ## *  When running the command `make build`, there is a bunch of errors which end with the following lines:
 make build
